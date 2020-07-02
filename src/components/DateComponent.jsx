7f20547eed552ea41bdb3776dc481e1680958f3f
AnlianaAguilar <anlianaaguilar@gmail.com>
2020-07-02 12:10:43 -0400

    Campo hora

--- src/components/DateComponent.jsx
+++ src/components/DateComponent.jsx
@@ -3,16 +3,26 @@ import "../components/DateComponent.css";
 
 class DateComponent extends React.Component{
     render() {
+
+
       let today = new Date();
+      let hour = today.getHours();
+      let  min  = today.getMinutes();
       let month = today.getMonth() + 1;
       let year = today.getFullYear();
       let day  = today.getDate();
+      hour = (hour < 10 ? "0" : "") + hour;
+      min = (min < 10 ? "0" : "") + min;
       day = (day < 10 ? "0" : "") + day;
       month = (month < 10 ? "0" : "") + month;
+
       return(
         <div className="DateComponent">
-         { day + '/' + month + '/' + year }
+          { hour + ':' + min } <br/>
+          { day + '/' + month + '/' + year } 
+         
         </div>
+
       )
     }
   }
