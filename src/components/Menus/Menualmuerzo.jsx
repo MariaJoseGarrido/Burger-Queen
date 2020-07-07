0b974492c7f2d452d11a10a2144a239ac518bbde
MariaJoseGarrido <mjgarrido.h@gmail.com>
2020-07-07 16:51:53 -0400

    arreglos css

--- src/components/Menus/Menualmuerzo.jsx
+++ src/components/Menus/Menualmuerzo.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
+import backButton from "../../img/backButton.png"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "../Menus/Menu.css";
-import "../Rol/Cliente.css"
+import Waiter from "../../components/Rol/Waiter"
+import Cliente from "../../components/Rol/Cliente"
+import "../Menus/Menu.css"
 import simple from "../../img/almuerzo/simple.jpeg";
 import doble from "../../img/almuerzo/doble.jpeg";
 import cesar from "../../img/almuerzo/cesar.jpg";
 import limonada from "../../img/almuerzo/limonada.jpeg";
 import wrap from "../../img/almuerzo/wrap.jpeg";
-import jugo from "../../img/jugo.jpeg";
-import nuggets from "../../img/almuerzo/nuggets.jpeg"
-import cerveza from "../../img/almuerzo/cerveza.jpeg"
-import nachos from "../../img/almuerzo/nachos.jpeg"
-import aritos from "../../img/almuerzo/aritos.jpg"
-import soda from "../../img/almuerzo/soda.jpeg"
-import papas from "../../img/almuerzo/papas.jpeg"
-import Cliente from "../../components/Rol/Cliente"
-import Waiter from "../../components/Rol/Waiter"
-import backButton from "../../img/backButton.png"
-
+import jugo from "../../img/jugo.jpeg"
+import nuggets from "../../img/almuerzo/nuggets.jpeg";
+import cerveza from "../../img/almuerzo/cerveza.jpeg";
+import nachos from "../../img/almuerzo/nachos.jpeg";
+import aritos from "../../img/almuerzo/aritos.jpg";
+import soda from "../../img/almuerzo/soda.jpeg";
+import papas from "../../img/almuerzo/papas.jpeg";
 
 class Menualmuerzo extends Component {
     backButton = () => { 
@@ -44,68 +42,124 @@ class Menualmuerzo extends Component {
 					/>
                    
 				</Link> 
-				<Cliente />  
-                
+				<Cliente />      
 			</div>
-            <div class="container-sm">
-                <div className="title">
-                    <div>Para Comer</div>
-                    <div>Para Beber</div>
-                    <div>Para Acompañar</div>
-                </div>
-               
-                <div class="row">
-                    <div className="col"> <p>Burger Princess</p>                  
-                     <img className="simple"
-                        alt="simple" 
-                        src={simple} />	</div>	
-                    <div className="col"> <p>Cerveza</p>
-                    <img className="cerveza"
-                        alt="cerveza" 
-                        src={cerveza} /></div>
-                    <div className="col"> <p>Nachos</p>
-                    <img className="nachos"
-                        alt="nachos" 
-                        src={nachos} /></div>    
-                    <div className="w-100"></div>
-                    <div className="col"><p> Burger Queen</p>
-                    <img className="doble"
-                        alt="doble" 
-                        src={doble} /></div>
-                    <div className="col"><p>Limonada</p>
-                    <img className="limonada"
-                        alt="limonada" 
-                        src={limonada} /></div>
-                       <div className="col"> <p>Aritos de cebolla</p>
-                    <img className="aritos"
-                        alt="aritos" 
-                        src={aritos} /></div>    
-                    <div className="w-100"></div>
-                    <div className="col"><p>Caesar Shrimp Bowl</p>
-                    <img className="cesar"
-                        alt="cesar" 
-                        src={cesar} /></div>
-                    <div className="col"><p>Bebida</p>
-                    <img className="soda"
-                        alt="soda" 
-                        src={soda} /></div>
-                        <div className="col"> <p>Papas Fritas</p>
-                    <img className="papas"
-                        alt="papas" 
-                        src={papas} /></div>   
-                    <div className="w-100"></div>
-                    <div className="col"><p>Wrap</p>
-                    <img className="wrap"
-                        alt="wrap" 
-                        src={wrap} /></div>
-                    <div className="col"><p>Jugo de frutas natural</p>
-                    <img className="jugo"
-                        alt="jugo" 
-                        src={jugo} /></div>    
-                        <div className="col"> <p>Nuggets</p>
-                    <img className="nuggets"
-                        alt="nuggets" 
-                        src={nuggets} /></div>   
+            <div className="container-sm">
+                <div className="row">
+                    {/* PARA COMER  */}
+                    <div className="col-xl-4 row p-3 back">
+                        <div className="col-12 title ">
+                            <h1 className='title-menu'>Para Comer</h1>
+                        </div>
+                        <div className="col-xl-12 col-md-6"> 
+                            <p>Burger Princess</p>                  
+                            <div className="card-img">
+                                <img className="simple d-flex"
+                                alt="simple" 
+                                src={simple} />	
+                            </div>
+                        </div>
+                        <div className="col-xl-12 col-md-6">
+                            <p> Burger Queen</p>
+                            <div className="card-img">
+                                <img className="doble d-flex"
+                                    alt="doble" 
+                                    src={doble} />
+                            </div>
+                        </div>
+                        <div className="col-xl-12 col-md-6">
+                            <p>Caesar Shrimp Bowl</p>
+                            <div className="card-img">
+                                <img className="cesar d-flex"
+                                alt="cesar" 
+                                src={cesar} />
+                            </div>
+                        </div>
+                        <div className="col-xl-12 col-md-6">
+                            <p>Wrap</p>
+                            <div className="card-img">
+                                <img className="wrap d-flex"
+                                alt="wrap" 
+                                src={wrap} />
+                            </div>
+                        </div>
+                    </div>
+                    {/* PARA BEBER */}
+                    <div className="col-xl-4 row p-3 back">
+                        <div className="col-12 title ">
+                            <h1 className='title-menu'>Para Beber</h1>
+                        </div>
+                        <div className="col-xl-12 col-md-6"> 
+                            <p>Cerveza</p>                  
+                            <div className="card-img">
+                                <img className="cerveza d-flex"
+                                alt="cerveza" 
+                                src={cerveza} />	
+                            </div>
+                        </div>
+                        <div className="col-xl-12 col-md-6">
+                            <p>Limonada</p>
+                            <div className="card-img">
+                                <img className="limonada d-flex"
+                                alt="limonada" 
+                                src={limonada} />
+                            </div>
+                        </div>
+                        <div className="col-xl-12 col-md-6">
+                            <p>Bebida</p>
+                            <div className="card-img">
+                                <img className="soda d-flex"
+                                alt="soda" 
+                                src={soda} />
+                            </div>
+                        </div>
+                        <div className="col-xl-12 col-md-6">
+                            <p>Jugo de frutas natural</p>
+                            <div className="card-img">
+                                <img className="jugo d-flex"
+                                alt="jugo" 
+                                src={jugo} />
+                            </div>
+                        </div>
+                    </div>
+                    {/* PARA ACOMPAÑAR */}
+                    <div className="col-xl-4 row p-3 back">
+                        <div className="col-12 title ">
+                            <h1 className='title-menu'>Para Acompañar</h1>
+                        </div>
+                        <div className="col-xl-12 col-md-6"> 
+                            <p>Nachos</p>                  
+                            <div className="card-img">
+                                <img className="nachos d-flex"
+                                alt="nachos" 
+                                src={nachos} />	
+                            </div>
+                        </div> 
+                        <div className="col-xl-12 col-md-6">
+                            <p>Aritos de cebolla</p>
+                            <div className="card-img">
+                                <img className="aritos d-flex"
+                                alt="aritos" 
+                                src={aritos} />
+                            </div>
+                        </div>
+                        <div className="col-xl-12 col-md-6">
+                            <p>Papas fritas</p>
+                            <div className="card-img">
+                                <img className="papas d-flex"
+                                alt="papas" 
+                                src={papas} />
+                            </div>
+                        </div>
+                        <div className="col-xl-12 col-md-6">
+                            <p>Nuggets</p>
+                            <div className="card-img">
+                                <img className="nuggets d-flex"
+                                alt="nuggets" 
+                                src={nuggets} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             </React.Fragment>
--- src/components/Menus/Menudesayuno.jsx
+++ src/components/Menus/Menudesayuno.jsx
@@ -38,43 +38,73 @@ class Menudesayuno extends Component {
 					/>
                    
 				</Link> 
-				<Cliente />  
-                
+				<Cliente />      
 			</div>
-            <div class="container-sm">
-                <div className="title">
-                    <div>Para Comer</div>
-                    <div>Para Beber</div>
-                </div>
-                <div className="row">
-                    <div className="col"> <p>Sandwich Jamón Queso $</p>                  
-                     <img className="jamonqueso"
-                        alt="jamonqueso" 
-                        src={jamonqueso} />	</div>	
-                    <div className="col"> <p>Café Americano $</p>
-                    <img className="americano"
-                        alt="americano" 
-                        src={americano} /></div>
-                    <div className="w-100"></div>
-                    <div className="col"><p> Croissant $ </p>
-                    <img className="croissant"
-                        alt="croissant" 
-                        src={croissant} /></div>
-                    <div className="col"><p>Latte $</p>
-                    <img className="latte"
-                        alt="latte" 
-                        src={latte} /></div>
-                    <div className="w-100"></div>
-                    <div className="col"><p>Muffin $</p>
-                    <img className="muffin"
-                        alt="muffin" 
-                        src={muffin} /></div>
-                    <div className="col"><p>Jugo de frutas natural $</p>
-                    <img className="jugo"
-                        alt="jugo" 
-                        src={jugo} /></div>
-                </div>
+            <div className="container-sm">
+<div className="row">
+    {/* PARA COMER  */}
+    <div className="col-xl-4 row p-3 back">
+        <div className="col-12 title ">
+            <h1 className='title-menu'>Para Comer</h1>
+        </div>
+        <div className="col-xl-12 col-md-6"> 
+            <p>Sándwich Jamón-Queso</p>                  
+            <div className="card-img">
+                <img className="jamonqueso d-flex"
+                alt="jamonqueso" 
+                src={jamonqueso} />	
             </div>
+        </div>
+        <div className="col-xl-12 col-md-6">
+            <p> Croissant</p>
+            <div className="card-img">
+                <img className="Croissant d-flex"
+                    alt="croissant" 
+                    src={croissant} />
+            </div>
+        </div>
+        <div className="col-xl-12 col-md-6">
+            <p>Muffin</p>
+            <div className="card-img">
+                <img className="Muffin d-flex"
+                alt="muffin" 
+                src={muffin} />
+            </div>
+        </div>
+    </div>
+    {/* PARA BEBER */}
+    <div className="col-xl-4 row p-3 back">
+        <div className="col-12 title ">
+            <h1 className='title-menu'>Para Beber</h1>
+        </div>
+        <div className="col-xl-12 col-md-6"> 
+            <p>Café Americáno</p>                  
+            <div className="card-img">
+                <img className="Café Americano d-flex"
+                alt="americano" 
+                src={americano} />	
+            </div>
+        </div>
+        <div className="col-xl-12 col-md-6">
+            <p>Latte</p>
+            <div className="card-img">
+                <img className="latte d-flex"
+                alt="latte" 
+                src={latte} />
+            </div>
+        </div>
+        <div className="col-xl-12 col-md-6">
+            <p>Jugo de frutas natural</p>
+            <div className="card-img">
+                <img className="jugo d-flex"
+                alt="jugo" 
+                src={jugo} />
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+            
             </React.Fragment>
         );
     }
@@ -83,23 +113,4 @@ class Menudesayuno extends Component {
 export default Menudesayuno;
 
 
-
-// //Init firestore
-// var db = firebase.firestore();
-
-// //Agregar menu
-
-// function guardar(){
-//   let toDrink = document.getElementById('inputtoDrink').value;
-//   let toEat = document.getElementById('inputtoEat').value;
-//   let toAdd = document.getElementById('inputtoAdd').value;
-
-
-// db.collection("menu").add({
-//     fechaPublicacion: Date.now(),
-//     paraBeber: toDrink,
-//     paraComer: toEat,
-//     acompaniamiento: toAdd,
-//     userId: user.uid,
-// })
 
