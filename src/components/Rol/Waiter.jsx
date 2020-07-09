dca7727b255259fbec485d9414938088b1e223a1
alejandra <alejandra.colicheo17@gmail.com>
2020-07-09 13:00:20 -0400

    borramos botones antiguos de waiter

--- src/components/Rol/Waiter.jsx
+++ src/components/Rol/Waiter.jsx
@@ -1,63 +1,50 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Button from "../../components/button";
+
 import waitress from "../../img/waitress.jpeg";
-import "../Rol/waiter.css"
-import DateComponent from "../DateComponent"
-import backButton from "../../img/backButton.png"
-import Inicio from "../Inicio"
-import Menu from "../../components/NuevoMenu"
+import "../Rol/waiter.css";
+import DateComponent from "../DateComponent";
+import backButton from "../../img/backButton.png";
+import Inicio from "../Inicio";
+import Menu from "../../components/NuevoMenu";
 
 class welcomeWaiter extends Component {
-  backButton = () => { 
-		console.log('clicked back button')   
-		return(
-		<Router>
-		  <Route exact path="/Waiter">
-			  <Inicio />
-		  </Route>
-		</Router>
-		);
-    }
+  backButton = () => {
+    console.log("clicked back button");
+    return (
+      <Router>
+        <Route exact path="/Waiter">
+          <Inicio />
+        </Route>
+      </Router>
+    );
+  };
   render() {
     return (
       <Fragment>
         <div className="WaiterDiv">
-				<Link to="/inicio">
-                    <img 
-                        onClick={() => this.backButton()}
-						alt="backButton"
-						src={backButton}
-						className="backButton"
-					/>
-                   
-				</Link>
-                
-			</div>
+          <Link to="/inicio">
+            <img
+              onClick={() => this.backButton()}
+              alt="backButton"
+              src={backButton}
+              className="backButton"
+            />
+          </Link>
+        </div>
         <DateComponent />
-          
-          <div className="container-custom">
-            <div className="logo">
-              <img className="waitress" alt="waitress" src={waitress} />	
-              <h1 className="title-custom">¿Qué servicio vas a prestar?</h1>
-              <div>
-                <div className='button-welcome'>
-                  <Link to="/Menudesayuno"><Button  title="Desayuno" /></Link>
-                </div>
-                <div className='button-welcome'>
-                  <Link to="/Menualmuerzo"><Button className="button" title="Almuerzo" /></Link>
-                </div>
-              </div>
-            </div>
-          </div>
 
-          <div> 
-            <Menu />
+        <div className="container-custom">
+          <div className="logo">
+            <img className="waitress" alt="waitress" src={waitress} />
+            <h1 className="title-custom">¿Qué servicio vas a prestar?</h1>
           </div>
+        </div>
+
+        <Menu />
       </Fragment>
     );
   }
 }
 
 export default welcomeWaiter;
-
