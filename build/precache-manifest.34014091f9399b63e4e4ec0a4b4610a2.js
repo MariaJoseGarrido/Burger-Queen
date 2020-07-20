bba6e69be07eb7425b2930ec38dbbc7a86c7221a
MariaJoseGarrido <mjgarrido.h@gmail.com>
2020-07-20 17:16:27 -0400

    borrando comentarios, sacando extras

---
+++ build/precache-manifest.34014091f9399b63e4e4ec0a4b4610a2.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9480a9a7e3c4b277076e7f6ad8245db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd18fb2232dae1da90d",
+    "url": "/static/css/main.7c86e348.chunk.css"
+  },
+  {
+    "revision": "a2a6e91cc07539c9313a",
+    "url": "/static/js/0.d19e7df5.chunk.js"
+  },
+  {
+    "revision": "add6bef4fedd8f5e853d38a08fb657d7",
+    "url": "/static/js/0.d19e7df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d4a6acb34c639d1b98",
+    "url": "/static/js/10.de7d006b.chunk.js"
+  },
+  {
+    "revision": "6a8c74c96d4d40d5e87ab4543287bac3",
+    "url": "/static/js/10.de7d006b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09038f2fe9e3d8d0ddd",
+    "url": "/static/js/11.1fe6c4e3.chunk.js"
+  },
+  {
+    "revision": "6a8c74c96d4d40d5e87ab4543287bac3",
+    "url": "/static/js/11.1fe6c4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3603bfa85f7ccc019e6c",
+    "url": "/static/js/3.f735feca.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/3.f735feca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bceab1b6b565fe8ff1e1",
+    "url": "/static/js/4.2c20d8ae.chunk.js"
+  },
+  {
+    "revision": "7feb36c6c39f703ee09d65c567036575",
+    "url": "/static/js/4.2c20d8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584478b605ada4600358",
+    "url": "/static/js/5.3dfba044.chunk.js"
+  },
+  {
+    "revision": "4e2c02fcb88b596eaa92de7b97a0e735",
+    "url": "/static/js/5.3dfba044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcc9443750f198a1a4ce",
+    "url": "/static/js/6.216dd427.chunk.js"
+  },
+  {
+    "revision": "6a8c74c96d4d40d5e87ab4543287bac3",
+    "url": "/static/js/6.216dd427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8538fa6501242e1fdfe3",
+    "url": "/static/js/7.ad2a9b7e.chunk.js"
+  },
+  {
+    "revision": "add6bef4fedd8f5e853d38a08fb657d7",
+    "url": "/static/js/7.ad2a9b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ebd15cb6ca3ccd49064",
+    "url": "/static/js/8.5641618d.chunk.js"
+  },
+  {
+    "revision": "add6bef4fedd8f5e853d38a08fb657d7",
+    "url": "/static/js/8.5641618d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c5e08814605452ac09",
+    "url": "/static/js/9.b75c6512.chunk.js"
+  },
+  {
+    "revision": "add6bef4fedd8f5e853d38a08fb657d7",
+    "url": "/static/js/9.b75c6512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd18fb2232dae1da90d",
+    "url": "/static/js/main.3976310d.chunk.js"
+  },
+  {
+    "revision": "ea73583cae2c5d83e1e8",
+    "url": "/static/js/runtime-main.5f761fc2.js"
+  },
+  {
+    "revision": "2e819b3d6fd1150bab12bc3c405d40f3",
+    "url": "/static/media/americano.2e819b3d.jpeg"
+  },
+  {
+    "revision": "050e055b37c2ff9694c79de2d5807496",
+    "url": "/static/media/aritos.050e055b.jpg"
+  },
+  {
+    "revision": "2ac94d2b6dc77a7421390bc77e55be58",
+    "url": "/static/media/backButton.2ac94d2b.png"
+  },
+  {
+    "revision": "f1a27a006659aea1712d330f71181f20",
+    "url": "/static/media/bannerGarzon.f1a27a00.png"
+  },
+  {
+    "revision": "c90c3f32fdb083562a0e281da458cde6",
+    "url": "/static/media/bannercocina.c90c3f32.png"
+  },
+  {
+    "revision": "bb2bce6ee27a53f8ef3ae8a11c583a24",
+    "url": "/static/media/burgerlogo.bb2bce6e.png"
+  },
+  {
+    "revision": "9c9aaade62f6cddb70faf19acfe33ddf",
+    "url": "/static/media/cerveza.9c9aaade.jpeg"
+  },
+  {
+    "revision": "ed4c0f7f3284b232b1850a0bbc016842",
+    "url": "/static/media/cesar.ed4c0f7f.jpg"
+  },
+  {
+    "revision": "0538d146e81bb5f3d23e8e420a253cbb",
+    "url": "/static/media/croissant.0538d146.jpeg"
+  },
+  {
+    "revision": "b758f7bc8f00274d41b3c8b71d180ece",
+    "url": "/static/media/doble.b758f7bc.jpeg"
+  },
+  {
+    "revision": "cf2074dbb7b9245ab60bf30bbcf30386",
+    "url": "/static/media/jamonqueso.cf2074db.jpeg"
+  },
+  {
+    "revision": "34897535dccb62696e5764dba9c10b83",
+    "url": "/static/media/jugo.34897535.jpeg"
+  },
+  {
+    "revision": "76c3c5a271fb716ba45928f494e23025",
+    "url": "/static/media/latte.76c3c5a2.jpeg"
+  },
+  {
+    "revision": "cd10b5aacc1624c7c21dfc2491e9dbe9",
+    "url": "/static/media/limonada.cd10b5aa.jpeg"
+  },
+  {
+    "revision": "2789f2bed47bc155608805a2f019680f",
+    "url": "/static/media/muffin.2789f2be.jpeg"
+  },
+  {
+    "revision": "976a82042f3b527aad2fe26dcdcddcc5",
+    "url": "/static/media/nachos.976a8204.jpeg"
+  },
+  {
+    "revision": "223a630aa8dd02083199b833d29cc24a",
+    "url": "/static/media/nuggets.223a630a.jpeg"
+  },
+  {
+    "revision": "8a56d52b4cdd3fdf3f7e33b985700045",
+    "url": "/static/media/papas.8a56d52b.jpeg"
+  },
+  {
+    "revision": "8c049fdf418c0a9fdef43d7b17eb0ce0",
+    "url": "/static/media/simple.8c049fdf.jpeg"
+  },
+  {
+    "revision": "1466636802f221cbcf8335bc5a446bed",
+    "url": "/static/media/soda.14666368.jpeg"
+  },
+  {
+    "revision": "34b77128dd812a410d742a14267d7950",
+    "url": "/static/media/wrap.34b77128.jpeg"
+  }
+]);
